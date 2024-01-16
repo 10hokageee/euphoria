bd7ab2aa105234e669f5b943eb1ab8b55a2aaf4c
10hokageee <nikita.serdyukov.20033002@gmail.com>
2024-01-17 01:54:43 +0200

    added sections

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,13 @@
 $(function(){
-
+ $('.promo__slider').slick({
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  dots: true,
+  infinity: true,
+  fade: true,
+  cssEase: 'linear',
+  appendDots: '.dots-slider',
+  nextArrow: '<button type="button" class="slick-next"><svg><use xlink:href="images/sprite.svg#promo-arrow"></use></svg></button>',
+  prevArrow: '<button type="button" class="slick-prev"><svg><use xlink:href="images/sprite.svg#promo-arrow"></use></svg></button>'
+ });
 });

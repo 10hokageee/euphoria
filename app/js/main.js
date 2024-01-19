9bc3a5b9423509ed15c00b423917f8b54a40c0e6
10hokageee <nikita.serdyukov.20033002@gmail.com>
2024-01-20 01:00:55 +0200

    added product

--- app/js/main.js
+++ app/js/main.js
@@ -82,6 +82,26 @@ $(function(){
     $(this).toggleClass('filters__name--active')
   })
 
+  $('.product-slide__thumb').slick({
+   asNavFor: '.product-slide__big',
+   focusOnSelect: true,
+   slidesToShow: 3,
+   slidesToScroll: 1,
+   vertical: true,
+   draggable: false,
+  });
+
+  $('.product-slide__big').slick({
+    asNavFor: '.product-slide__thumb',
+    draggable: false,
+    fade: true,
+    cssEase: 'linear',
+    arrows: true,
+    appendArrows: '.product-slide__arrows',
+    nextArrow: '<button type="button" class="slick-next"><svg><use xlink:href="images/sprite.svg#product-slide-arrow"></use></svg></button>',
+    prevArrow: '<button type="button" class="slick-prev"><svg><use xlink:href="images/sprite.svg#product-slide-arrow"></use></svg></button>'
+  });
+
 });
 
 
